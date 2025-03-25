8623aa3bf6622e2f33271a7af1809fe3c2bfb5db
psawner <adventure03ps@gmail.com>
2025-03-26 01:00:46 +0530

    Delete folder1 directory

--- folder1/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var typed = new Typed(".text", {
-        strings: ["Frontend Developer", "Web Designer"],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true
-    });
-});
